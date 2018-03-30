608eb37f76001529c75cda872c03913c7bdba0c7
Christopher Wong <Christopher.Wong.1@stonybrook.edu>
2018-03-30 12:48:00 -0400

    finished katana

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Microsoft.Owin.Hosting;
 using Owin;
 
 namespace KatanaIntro
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string uri = "http://localhost:8080";
-            using (WebApp.Start<Startup>(uri))
-            {
-                Console.WriteLine("Started!");
-                Console.ReadKey();
-                Console.WriteLine("Stopped.");
-            }
-        }
-    }
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string uri = "http://localhost:8080";
+    //        using (WebApp.Start<Startup>(uri))
+    //        {
+    //            Console.WriteLine("Started!");
+    //            Console.ReadKey();
+    //            Console.WriteLine("Stopped.");
+    //        }
+    //    }
+    //}
 
     public class Startup
     {
@@ -31,10 +34,64 @@ namespace KatanaIntro
         /* ... more methods that app will have */
         public void Configuration(IAppBuilder app)
         {
-            app.Run(ctx =>
+            //app.Use<HelloWorldComponent>();
+            //SetUpServer(app);
+            //app.Use(async(environment, next) =>
+            //{
+            //    foreach (var pair in environment.Environment)
+            //    {
+            //        Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+            //    }
+                
+
+            //    await next();
+            //});
+            app.Use(async (environment, next) =>
             {
-                return ctx.Response.WriteAsync("Hello World"); 
+                Console.WriteLine("requesting : " + environment.Request.Path);
+                await next();
+                Console.WriteLine("response : " + environment.Response.StatusCode);
             });
+            ConfigureWebApi(app);
+            app.UseHelloWorld();
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("DefaultApi","api/{controller}/{id}", new { id = RouteParameter.Optional});
+            app.UseWebApi(config);
+        }
+
+        private static void SetUpServer(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+            app.Run(ctx => { return ctx.Response.WriteAsync("Hello World"); });
+        }
+    }
+
+    public static class AppBuilderExtensions
+    {
+        public static void UseHelloWorld(this IAppBuilder app)
+        {
+            app.Use<HelloWorldComponent>();
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!!");
+            }
         }
     }
 }
